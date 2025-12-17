2a8272e4db128b8a333554ce0f7c191475bce418
Kosarev Vsevolod <iamvkosarev@gmail.com>
2025-12-18 02:34:48 +0800

    Add load envs

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"github.com/iamvkosarev/book-shelf/config"
 	"github.com/iamvkosarev/book-shelf/internal/app"
+	"github.com/joho/godotenv"
 	"log"
 )
 
 func main() {
+	godotenv.Load()
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatalf("loading config error: %s\n", err)
